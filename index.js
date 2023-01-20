ccae139c6900b9164c5b1643b2b53f7ce4a8022a
Antosha Baboshin <antosha.js@gmail.com>
2023-01-20 18:16:27 +0300

    feat: aupdate toggleMenu

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const NAV_LINKS = document.querySelectorAll('.nav-link');
-const HEADER_NAV_LIST = document.querySelector('.header-nav-list');
+const BURGER = document.getElementById('burger');
+const BODY = document.getElementsByTagName('body')[0];
+const MENU_WRAPPER = document.querySelector('.nav-list-wrapper');
 
-const closeMenu = () => {
-  document.getElementById('burger').checked = false;
+const toggleMenu = () => {
+  if (!BURGER.checked) {
+    BODY.style.overflow = 'auto';
+    MENU_WRAPPER.style.display = 'none';
+  } else {
+    BODY.style.overflow = 'hidden';
+    MENU_WRAPPER.style.display = 'block';
+  }
 }
 
 NAV_LINKS.forEach((el) => {
-  el.addEventListener('click', closeMenu);
-})
+  el.addEventListener('click', () => {
+    BURGER.checked = false;
+    toggleMenu();
+  });
+});
+BURGER.addEventListener('click', toggleMenu);
+MENU_WRAPPER.addEventListener('click', () => {
+  BURGER.checked = false;
+  toggleMenu();
+});
